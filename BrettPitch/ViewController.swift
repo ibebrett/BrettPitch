7456f7004c4f61ae9d1ae09b1ea6249b3822ee03
Brett Jurman <brettjurman@Bretts-Air.home>
2015-02-26 08:12:23 -0500

    Initial Commit

---
+++ BrettPitch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrettPitch
+//
+//  Created by Brett Jurman on 2/26/15.
+//  Copyright (c) 2015 Brett Jurman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
