f70751a0e38566b5e9b38bc0d0e780f68e6bcf19
Brett Jurman <brettjurman@Bretts-Air.home>
2015-02-26 10:28:28 -0500

    Initial commit of up to the beginning of lesson 2

--- BrettPitch/ViewController.swift
+++ BrettPitch/ViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var recordingInProgress: UILabel!
+    @IBOutlet weak var stopButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
+    @IBAction func recordAudio(sender: UIButton) {
+        recordingInProgress.hidden = false;
+    }
 }
 
